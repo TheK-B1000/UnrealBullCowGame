f12bb8e9735038f1d83e61644e775b5a99730648
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2020-02-19 12:16:36 -0800

    02 - FStrings and TEXT() macro

--- BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,11 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome to Bull Cow Game!");
-    PrintLine("Press Enter to Continue...");
+    PrintLine(TEXT("Welcome to Bull Cow Game!"));
+    PrintLine(TEXT("Press Enter to Continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+    FString HiddenWord = TEXT("ape");
 }
