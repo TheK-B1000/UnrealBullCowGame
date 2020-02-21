a28c32f7a49aa85cc20b144d4f2bf3cc1b83cc1b
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2020-02-20 17:51:41 -0800

    16 - TArray Functions Adding And Removing

--- BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,11 @@
 // Social Cube Media 2020
 #include "BullCowCartridge.h"
+#include "Public/HiddenWordList.h"
 
-void UBullCowCartridge::BeginPlay() // When the game starts
+void UBullCowCartridge::BeginPlay() 
 {
     Super::BeginPlay();
-    SetupGame(); // Setting up game
-
-    PrintLine(TEXT("The HiddenWord is: %s. \nIt is %i charactes long."), *HiddenWord, HiddenWord.Len()); // Debug line
+    SetupGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
@@ -46,47 +45,62 @@ void UBullCowCartridge::EndGame()
 
 void UBullCowCartridge::ProcessGuess(FString Guess)
 {
-      if (Guess == HiddenWord) // Is guess Correct?
-      {
-          PrintLine(TEXT("You Won!"));
-          EndGame();
-          return;
-      }
+    if (Guess == HiddenWord) // Is guess Correct?
+    {
+        PrintLine(TEXT("You Won!"));
+        EndGame();
+        return;
+    }
 
-      if (Guess.Len() != HiddenWord.Len()) // Check Right Number Of Characters 
-      {
-          PrintLine(TEXT("Sorry wrong number of characters, try guessing again!"));
-          PrintLine(TEXT("The word is %i characters long"), HiddenWord.Len());
-          PrintLine(TEXT("You have %i lives remaining"), Lives);
-          return;
-      }
+    if (Guess.Len() != HiddenWord.Len()) // Check Right Number Of Characters 
+    {
+        PrintLine(TEXT("Sorry wrong number of characters, try guessing again!"));
+        PrintLine(TEXT("The word is %i characters long"), HiddenWord.Len());
+        PrintLine(TEXT("You have %i lives remaining"), Lives);
+        return;
+    }
 
-      /// Check If Isogram
-      if (!IsIsogram(Guess))
-      {
-          PrintLine(TEXT("Please enter an Isogram! A word without repeating letters!"));
-          PrintLine(TEXT("You have lost a live! You have %i lives remaining!"), Lives--);    // Remove Life & Show Lives Left
-          return;
-      }
+    /// Check If Isogram
+    if (!IsIsogram(Guess))
+    {
+        PrintLine(TEXT("Please enter an Isogram! A word without repeating letters!"));
+        PrintLine(TEXT("You have lost a live! You have %i lives remaining!"), Lives--);    // Remove Life & Show Lives Left
+        return;
+    }
 
       if (Lives <= 0)
       {
+          ClearScreen();
           PrintLine(TEXT("You have no lives left!"));
           PrintLine(TEXT("The hidden word was: %s"), *HiddenWord);
           EndGame();
           return;
       }
-
     PrintLine(TEXT("Trying guessing again, you have %i lives left"), Lives);
+}
+
+TArray<FString> UBullCowCartridge::CheckValidWords(TArray<FString> WordsList) const
+{
+    TArray<FString> ValueWords;
+
+    for (auto Index = 0; Index < 10; Index++)
+    {
+        if (WordsList[Index].Len() >= 4 && WordsList[Index].Len() <= 8)
+        {
+            if (IsIsogram(WordsList[Index]))
+            {
+                ValueWords.Emplace(WordsList[Index]);
+            }
+        }
+    }
+    return ValueWords;
 }
 
 bool UBullCowCartridge::IsIsogram(FString Word) const
 {
-    int32 Index = 0;
-
-    for (int32 Comparison = Index + 1; Comparison < Word.Len(); Comparison++)
+    for (auto Index = 0; Index < Word.Len(); Index++)
     {
-        for (auto Index = 0; Index < Word.Len(); Index++)
+        for (int32 Comparison = Index + 1; Comparison < Word.Len(); Comparison++)
         {
             if (Word[Index] == Word[Comparison])
             {
